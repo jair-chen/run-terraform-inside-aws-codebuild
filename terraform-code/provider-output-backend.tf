bc643bfef5c6f86dafa7e3cb8abd5ca8c6955851
jair-chen <jchen@aviatrix.com>
2023-03-24 16:31:21 +0800

    update

--- terraform-code/provider-output-backend.tf
+++ terraform-code/provider-output-backend.tf
@@ -1,6 +1,6 @@
 provider "aws" {
 #  shared_credentials_file  = "${var.cred-file}" # we don't use it in CodeBuild
-  profile    = "${var.profile}"
+#  profile    = "${var.profile}"
   region     = "${var.region}"
 }
 
